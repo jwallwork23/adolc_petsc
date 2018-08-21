e956c8262405b9b38133083487f192d476f22a6a
jwallwork23 <jwallwork16@imperial.ac.uk>
2018-08-21 15:07:59 -0500

    TODOs and tidying. Strange error when generalising Jacobian calculation

--- petsc/ts/ex16adj.cpp
+++ petsc/ts/ex16adj.cpp
@@ -103,7 +103,7 @@ static PetscErrorCode RHSSubJacobian(TS ts,PetscReal t,Vec X,Mat A,Mat B,PetscIn
   PetscInt          s = sizeof(indep_cols)/sizeof(indep_cols[0]);
   PetscScalar       J[m][s];
   PetscScalar       **Jx;
-  PetscScalar       *row0,*row1;
+  PetscScalar       *row0,*row1;			// TODO: how to do this more generally?
   const PetscScalar *x;
 
   PetscFunctionBeginUser;
@@ -113,21 +113,22 @@ static PetscErrorCode RHSSubJacobian(TS ts,PetscReal t,Vec X,Mat A,Mat B,PetscIn
   const PetscScalar *ptr_to_indep = indep_vars;		// Give a pointer
 
   ierr = PetscMalloc1(m,&Jx);CHKERRQ(ierr);		// Allocate memory for Jacobian
-  ierr = PetscMalloc1(n,&row0);CHKERRQ(ierr);
-  ierr = PetscMalloc1(n,&row1);CHKERRQ(ierr);
-  Jx[0] = row0; Jx[1] = row1;
+  ierr = PetscMalloc1(n,&row0);CHKERRQ(ierr);		// TODO: how to do this more generally?
+  ierr = PetscMalloc1(n,&row1);CHKERRQ(ierr);		// TODO: ------------"-----------------
+  Jx[0] = row0; Jx[1] = row1;				// TODO: ------------"-----------------
   jacobian(1,m,n,ptr_to_indep,Jx);			// Calculate Jacobian using ADOL-C
   for(i=0; i<m; i++){
-    for(j=0; j<s; j++)
+    for(j=0; j<s; j++){
       J[i][j] = Jx[i][indep_cols[j]];
+    }
   }
-  for(i=0; i<s; i++)
+  for(i=0; i<s; i++)					// Shift column index subset
     indep_cols[i] = i;
   ierr = MatSetValues(A,m,row,s,indep_cols,&J[0][0],INSERT_VALUES);CHKERRQ(ierr);
 
   ierr = PetscFree(Jx);CHKERRQ(ierr);
-  ierr = PetscFree(row0);CHKERRQ(ierr);
-  ierr = PetscFree(row1);CHKERRQ(ierr);
+  ierr = PetscFree(row0);CHKERRQ(ierr);			// TODO: how to do this more generally?
+  ierr = PetscFree(row1);CHKERRQ(ierr);			// TODO: ------------"-----------------
   ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
   if (A != B) {
@@ -140,75 +141,28 @@ static PetscErrorCode RHSSubJacobian(TS ts,PetscReal t,Vec X,Mat A,Mat B,PetscIn
 
 static PetscErrorCode RHSJacobian(TS ts,PetscReal t,Vec X,Mat A,Mat B,void *ctx)
 {
-
   PetscErrorCode    ierr;
-  PetscInt          indep_cols[] = {0,1};
+  PetscInt          indep_cols[] = {0,1};		// Choose relevant independent variables
 
   PetscFunctionBeginUser;
   ierr = RHSSubJacobian(ts,t,X,A,B,indep_cols,ctx);CHKERRQ(ierr);
   PetscFunctionReturn(0);
-
-/*
-  PetscErrorCode    ierr;
-  User              user = (User)ctx;
-  const PetscReal   mu   = user->mu;
-  PetscInt          row[] = {0,1},col[] = {0,1,2},indep_cols[] = {0,1},i,j;
-  PetscInt          m = sizeof(row)/sizeof(row[0]);	// Number of dependent variables
-  PetscInt          n = sizeof(col)/sizeof(col[0]);	// Number of independent variables
-  PetscInt          s = sizeof(indep_cols)/sizeof(indep_cols[0]);
-  PetscScalar       J[m][s];
-  PetscScalar       **Jx;
-  PetscScalar       *row0,*row1;
-  const PetscScalar *x;
-
-  PetscFunctionBeginUser;
-  ierr = VecGetArrayRead(X,&x);CHKERRQ(ierr);
-
-  const PetscScalar indep_vars[3] = {x[0],x[1],mu};	// Concatenate independent vars
-  const PetscScalar *ptr_to_indep = indep_vars;		// Give a pointer
-
-  ierr = PetscMalloc1(m,&Jx);CHKERRQ(ierr);		// Allocate memory for Jacobian
-  ierr = PetscMalloc1(n,&row0);CHKERRQ(ierr);
-  ierr = PetscMalloc1(n,&row1);CHKERRQ(ierr);
-  Jx[0] = row0; Jx[1] = row1;
-  jacobian(1,m,n,ptr_to_indep,Jx);			// Calculate Jacobian using ADOL-C
-  for(i=0; i<m; i++){
-    for(j=0; j<s; j++)
-      J[i][j] = Jx[i][indep_cols[j]];
-  }
-  for(i=0; i<s; i++)
-    indep_cols[i] = i;
-  ierr = MatSetValues(A,m,row,s,indep_cols,&J[0][0],INSERT_VALUES);CHKERRQ(ierr);
-
-  ierr = PetscFree(Jx);CHKERRQ(ierr);
-  ierr = PetscFree(row0);CHKERRQ(ierr);
-  ierr = PetscFree(row1);CHKERRQ(ierr);
-  ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
-  ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
-  if (A != B) {
-    ierr = MatAssemblyBegin(B,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
-    ierr = MatAssemblyEnd(B,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
-  }
-  ierr = VecRestoreArrayRead(X,&x);CHKERRQ(ierr);  
-  PetscFunctionReturn(0);
-*/
 }
 
 static PetscErrorCode RHSJacobianP(TS ts,PetscReal t,Vec X,Mat A,void *ctx)
 {
-/*
+
   PetscErrorCode    ierr;
   PetscInt          indep_cols[] = {2};
-
-  PetscFunctionBeginUser;
+/*
+  PetscFunctionBeginUser;				// TODO: why does this version not work?
   ierr = RHSSubJacobian(ts,t,X,A,A,indep_cols,ctx);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 */
 
-  PetscErrorCode    ierr;
   User              user = (User)ctx;
   PetscReal         mu   = user->mu;
-  PetscInt          row[] = {0,1},col[]={0,1,2},indep_cols[]={2},i,j;
+  PetscInt          row[] = {0,1},col[] = {0,1,2},i,j;
   PetscInt          m = sizeof(row)/sizeof(row[0]);	// Number of dependent variables
   PetscInt          n = sizeof(col)/sizeof(col[0]);	// Number of independent variables
   PetscInt          s = sizeof(indep_cols)/sizeof(indep_cols[0]);
@@ -220,8 +174,8 @@ static PetscErrorCode RHSJacobianP(TS ts,PetscReal t,Vec X,Mat A,void *ctx)
   PetscFunctionBeginUser;
   ierr = VecGetArrayRead(X,&x);CHKERRQ(ierr);
 
-  const PetscScalar indep_vars[3] = {x[0],x[1],mu};
-  const PetscScalar *ptr_to_indep = indep_vars;
+  const PetscScalar indep_vars[3] = {x[0],x[1],mu};	// Concatenate independent vars
+  const PetscScalar *ptr_to_indep = indep_vars;		// Give a pointer
 
   ierr = PetscMalloc1(m,&Jp);CHKERRQ(ierr);		// Allocate memory for Jacobian
   ierr = PetscMalloc1(n,&row0);CHKERRQ(ierr);
@@ -229,10 +183,11 @@ static PetscErrorCode RHSJacobianP(TS ts,PetscReal t,Vec X,Mat A,void *ctx)
   Jp[0] = row0; Jp[1] = row1;
   jacobian(1,m,n,ptr_to_indep,Jp);			// Calculate Jacobian using ADOL-C
   for(i=0; i<m; i++){
-    for(j=0; j<s; j++)
+    for(j=0; j<s; j++){
       J[i][j] = Jp[i][indep_cols[j]];
+    }
   }
-  for(i=0; i<s; i++)
+  for(i=0; i<s; i++)					// Shift column index subset
     indep_cols[i] = i;
   ierr = MatSetValues(A,m,row,s,indep_cols,&J[0][0],INSERT_VALUES);CHKERRQ(ierr);
 
