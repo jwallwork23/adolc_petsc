11aa7ad450579a22c3802e4294f0a501ad066fa7
jwallwork23 <jwallwork16@imperial.ac.uk>
2018-08-31 11:32:21 -0500

    Ghost experiment setup

--- petsc/ts/adr/test.cpp
+++ petsc/ts/adr/test.cpp
@@ -56,10 +56,9 @@ PetscErrorCode aFieldFree(PetscInt xs,PetscInt ys,PetscInt xm,PetscInt ym,PetscI
 int main(int argc,char **argv)
 {
   PetscErrorCode  ierr;
-  aField          **A = NULL;
-  aField          *ad = NULL;
-  PetscInt        ls=0,lm=3,i,j,k=0,dof=2,gs,gm;
-  PetscScalar     *aa,c=0.;
+  aField          **A = NULL,**B = NULL;
+  aField          *ad = NULL,*bd = NULL;
+  PetscInt        ls=0,lm=3,i,j,dof=2,gs,gm;
   adouble         tmp;
   PetscBool       ghost=PETSC_FALSE;
 
@@ -68,19 +67,21 @@ int main(int argc,char **argv)
   ierr = PetscOptionsGetBool(NULL,NULL,"-ghost",&ghost,NULL);CHKERRQ(ierr);
   ierr = PetscOptionsGetInt(NULL,NULL,"-n",&lm,NULL);CHKERRQ(ierr);
   gs=ls-1,gm=lm+2;
+  Field           aa[lm][lm],bb[lm][lm];
 
   //ierr = aFieldMalloc(gs,gs,gm,gm,2,&a);CHKERRQ(ierr);
-  ierr = PetscMalloc1(2*lm*lm,&aa);CHKERRQ(ierr);
 
+  printf("Before:\n\n");
   for (j=ls; j<lm; j++) {
     for (i=ls; i<lm; i++) {
-      aa[k] = k;k++;
-      aa[k] = k;k++;
+      aa[i][j].u = i;
+      aa[i][j].v = j;
+      printf("%f, %f\n",aa[i][j].u,aa[i][j].v);
     }
   }
 
 
-  // Allocate memory for 2-array aField
+  // Allocate memory for 2-array aFields
   if (ghost) {
     ad = new aField[gm*gm];
     A = new aField*[gm];
@@ -89,6 +90,14 @@ int main(int argc,char **argv)
       A[i] = ad + dof*i*gm - dof*gs;
     }
     A -= gs;
+
+    bd = new aField[gm*gm];
+    B = new aField*[gm];
+    for (i=gs; i<gm; i++) {
+      B[i] = new aField[gm];
+      B[i] = bd + dof*i*gm - dof*gs;
+    }
+    B -= gs;
    } else {
     ad = new aField[lm*lm];
     A = new aField*[lm];
@@ -97,39 +106,60 @@ int main(int argc,char **argv)
       A[i] = ad + i*lm - ls;
     }
     A -= ls;
+
+    bd = new aField[lm*lm];
+    B = new aField*[lm];
+    for (i=ls; i<lm; i++) {
+      B[i] = new aField[lm];
+      B[i] = bd + i*lm - ls;
+    }
+    B -= ls;
   }
 
   // Perform active section
-  k = 0;
   trace_on(1);
-  if (ghost) {
-    A[-1][0].u <<= c;
+  for (i=ls; i<lm; i++) {
+    for (j=ls; j<lm; j++) {
+      //printf("i=%d,j=%d\n",i,j);
+      //printf("  %d\n",&A[i][j]);
+      //printf("    %d\n",&A[i][j].v);
+      A[i][j].u <<= aa[i][j].u;
+      A[i][j].v <<= aa[i][j].v;
+    }
+  }
+  if (!ghost) {
+    ls += 1;
   }
   for (i=ls; i<lm; i++) {
     for (j=ls; j<lm; j++) {
-      printf("i=%d,j=%d\n",i,j);
-      printf("  %d\n",&A[i][j]);
-      printf("    %d\n",&A[i][j].v);
-      A[i][j].u <<= aa[k++];
-      A[i][j].v <<= aa[k--];
-
-      tmp = A[i][j].u;A[i][j].u = A[i][j].v;A[i][j].v = tmp;
-
-      A[i][j].u >>= aa[k++];
-      A[i][j].v >>= aa[k++];
+      B[i][j].u = A[i-1][j].u;
+      B[i][j].v = A[i][j-1].v;
     }
   }
-  if (ghost) {
-    A[-1][0].u >>= c;
+
+  if (!ghost) {
+    ls -= 1;
+  }
+  for (i=ls; i<lm; i++) {
+    for (j=ls; j<lm; j++) {
+      B[i][j].u >>= bb[i][j].u;
+      B[i][j].v >>= bb[i][j].v;
+    }
   }
   trace_off();
 
-  printf("Done. Now need to destroy and deallocate aField.\n");
+  printf("\nAfter:\n\n");
+  for (j=ls; j<lm; j++) {
+    for (i=ls; i<lm; i++) {
+      printf("%f, %f\n",bb[i][j].u,bb[i][j].v);
+    }
+  }
+
+  printf("\nDone. Now need to destroy and deallocate aField.\n");
   delete[] A;
   delete[] ad;
   printf("Done.\n");
 
-  ierr = PetscFree(aa);CHKERRQ(ierr);
   //ierr = aFieldFree(gs,gs,gm,gm,2,&a);CHKERRQ(ierr);
   ierr = PetscFinalize();
   return ierr;
