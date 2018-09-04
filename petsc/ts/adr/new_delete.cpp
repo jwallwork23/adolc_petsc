e9fcd1abbe0bd3fc77c690ff20475f5a80148769
jwallwork23 <jwallwork16@imperial.ac.uk>
2018-09-04 11:48:37 -0500

    Test case working so far. Periodic problem considered

--- petsc/ts/adr/new_delete.cpp
+++ petsc/ts/adr/new_delete.cpp
@@ -15,62 +15,137 @@ typedef struct {
 int main(int argc,char **argv)
 {
     PetscErrorCode ierr;
+    int            xss=0,yss=0,i,j;
+    PetscBool      ghost;
 
     ierr = PetscInitialize(&argc,&argv,(char*)0,NULL);if (ierr) return ierr;
+    ierr = PetscOptionsGetBool(NULL,NULL,"-ghost",&ghost,NULL);CHKERRQ(ierr);
+
 
     //Create an array of pointers that points to more arrays of Fields
-    Field** matrix = new Field*[5];
-    for (int j = 0; j < 5; j++) {
-        matrix[j] = new Field[5];
+    Field** matrix1 = new Field*[5];
+    for (j = 0; j < 5; j++) {
+        matrix1[j] = new Field[5];
     }
 
-    for (int j = 0; j < 5; j++) {
-        for (int i = 0; i < 5; i++) {
-            matrix[j][i].u = j*5 + i + 1;
-            matrix[j][i].v = i*5 + j + 1;
+    for (j = 0; j < 5; j++) {
+        for (i = 0; i < 5; i++) {
+            matrix1[j][i].u = (j*5 + i) % 2;
+            matrix1[j][i].v = (i*5 + j) % 2;
         }
     }
 
     //Print out the matrices to verify we have created the matrix
     cout << endl << "BEFORE" << endl << endl << "u component:" << endl;
-    for (int j = 0; j < 5; j++) {
-        for (int i = 0; i < 5; i++) {
-            cout << matrix[j][i].u << ", ";
+    for (j = 0; j < 5; j++) {
+        for (i = 0; i < 5; i++) {
+            cout << matrix1[j][i].u << ", ";
         }
         cout << endl;
     }
     cout << "v component:" << endl;
-    for (int j = 0; j < 5; j++) {
-        for (int i = 0; i < 5; i++) {
-            cout << matrix[j][i].v << ", ";
+    for (j = 0; j < 5; j++) {
+        for (i = 0; i < 5; i++) {
+            cout << matrix1[j][i].v << ", ";
         }
         cout << endl;
     }
     cout << endl;
 
     //Create an array of pointers that points to more arrays of aFields
-    aField** amatrix = new aField*[5];
-    for (int j = 0; j < 5; j++) {
-        amatrix[j] = new aField[5];
+    aField** amatrix1 = new aField*[5];
+    for (j = 0; j < 5; j++) {
+        amatrix1[j] = new aField[5];
     }
 
+    //Do the same for outputs
+    Field** matrix2 = new Field*[5];
+    for (j = 0; j < 5; j++) {
+        matrix2[j] = new Field[5];
+    }
 
+    //Do the same for outputs
+    aField** amatrix2 = new aField*[5];
+    for (j = 0; j < 5; j++) {
+        amatrix2[j] = new aField[5];
+    }
 
+    trace_on(1);
 
+    for (j = 0; j < 5; j++) {
+        for (i = 0; i < 5; i++) {
+            amatrix1[j][i].u <<= matrix1[j][i].u;
+            amatrix1[j][i].v <<= matrix1[j][i].v;
+        }
+    }
+
+    if (!ghost) {
+      xss += 1;yss += 1;
+    }
+    for (j=0; j<5; j++) {
+      for (i=xss; i<5; i++) {
+        amatrix2[j][i].u = amatrix1[j][i-1].u;
+      }
+    }
+    for (j=yss; j<5; j++) {
+      for (i=0; i<5; i++) {
+        amatrix2[j][i].v = amatrix1[j-1][i].v;
+      }
+    }
+
+    for (j = 0; j < 5; j++) {
+        for (i = 0; i < 5; i++) {
+            amatrix2[j][i].u >>= matrix2[j][i].u;
+            amatrix2[j][i].v >>= matrix2[j][i].v;
+        }
+    }
+
+    trace_off();
+
+    //Print out the matrices to verify everything has gone through
+    cout << endl << "AFTER" << endl << endl << "u component:" << endl;
+    for (j = 0; j < 5; j++) {
+        for (i = 0; i < 5; i++) {
+            cout << matrix2[j][i].u << ", ";
+        }
+        cout << endl;
+    }
+    cout << "v component:" << endl;
+    for (j = 0; j < 5; j++) {
+        for (i = 0; i < 5; i++) {
+            cout << matrix2[j][i].v << ", ";
+        }
+        cout << endl;
+    }
+    cout << endl;
 
     //Free each sub-array
-    for(int j = 0; j < 5; j++) {
-        delete[] matrix[j];   
+    for(j = 0; j < 5; j++) {
+        delete[] amatrix2[j];   
     }
     //Free the array of pointers
-    delete[] matrix;
+    delete[] amatrix2;
 
     //Free each sub-array
-    for(int j = 0; j < 5; j++) {
-        delete[] amatrix[j];   
+    for(j = 0; j < 5; j++) {
+        delete[] matrix2[j];   
     }
     //Free the array of pointers
-    delete[] amatrix;
+    delete[] matrix2;
+
+    //Free each sub-array
+    for(j = 0; j < 5; j++) {
+        delete[] amatrix1[j];   
+    }
+    //Free the array of pointers
+    delete[] amatrix1;
+
+    //Free each sub-array
+    for(j = 0; j < 5; j++) {
+        delete[] matrix1[j];   
+    }
+    //Free the array of pointers
+    delete[] matrix1;
 
     ierr = PetscFinalize();CHKERRQ(ierr);
 
