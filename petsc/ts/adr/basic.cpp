d263d62982f59ee32828b46b06229724d37b228f
jwallwork23 <jwallwork16@imperial.ac.uk>
2018-09-04 15:45:50 -0500

    Hint from Navjot

--- petsc/ts/adr/basic.cpp
+++ petsc/ts/adr/basic.cpp
@@ -3,26 +3,33 @@
 
 using namespace std;
 
+typedef struct {
+  PetscScalar u,v;
+} Field;
+
 int main()
 {
+  // 1-array
+  Field* e = new Field[1];
 
-  double** d = new double*[1];
-  d[0] = new double[1];
-  double* e = new double[1];
+  // 2-array
+  Field** d = new Field*[1];
+  d[0] = new Field[1];
 
   cout << "Initial addresses:" << endl;
   cout << "&d = " << &d[0][0] << endl;
   cout << "&e = " << &e[0] << endl << endl;
 
+  // set value in 2-array to value in 1-array
+  delete[] d[0];
   d[0] = e;
 
   cout << "Addresses after assignment:" << endl;
   cout << "&d = " << &d[0][0] << endl;
   cout << "&e = " << &e[0] << endl;
 
-  delete[] e;
-  //delete[] d[0];
   delete[] d;
+  delete[] e;
 
   return 0;
 }
