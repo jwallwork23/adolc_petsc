904b99404f42401655c6215c5bd360fd1d01bb44
jwallwork23 <jwallwork16@imperial.ac.uk>
2018-09-13 15:41:14 -0500

    Needs some debugging

--- petsc/colour/ex.c
+++ petsc/colour/ex.c
@@ -106,27 +106,29 @@ int main(int argc,char **args)
   /* Initialise the nonzero structure of the Jacobian TODO: use ADOL-C */
   ierr = FormJacobian(snes,u,J,J,&appctx);CHKERRQ(ierr);
 
-  // TODO: More to update
-
   /*
     Colour the matrix, i.e. determine groups of columns that share no common rows. These columns
     in the Jacobian can all be computed simultaneously.
   */
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"Creating colouring of J...\n");
   ierr = MatColoringCreate(J,&coloring);CHKERRQ(ierr);
   ierr = MatColoringSetType(coloring,MATCOLORINGSL);CHKERRQ(ierr);	// Use 'smallest last' method
   ierr = MatColoringSetFromOptions(coloring);CHKERRQ(ierr);
   ierr = MatColoringApply(coloring,&iscoloring);CHKERRQ(ierr);
-
+  ierr = MatColoringDestroy(&coloring);CHKERRQ(ierr);
   ierr = MatFDColoringCreate(J,iscoloring,&fdcoloring);CHKERRQ(ierr);
-  ierr = ISColoringDestroy(&iscoloring);CHKERRQ(ierr);
-  ierr = MatFDColoringSetFunction(fdcoloring,(PetscErrorCode(*)(void))FormFunction,&appctx);CHKERRQ(ierr);
   ierr = MatFDColoringSetFromOptions(fdcoloring);CHKERRQ(ierr);
+  ierr = MatFDColoringSetFunction(fdcoloring,(PetscErrorCode(*)(void))FormFunction,&appctx);CHKERRQ(ierr);
+  ierr = ISColoringDestroy(&iscoloring);CHKERRQ(ierr);
+  //ierr = MatFDColoringSetFromOptions(fdcoloring);CHKERRQ(ierr);
+
+  // MORE TODO: See mat/ex16
 
   /* Compute Jacobians this way using SNES */
   ierr = SNESSetJacobian(snes,J,J,SNESComputeJacobianDefaultColor,fdcoloring);CHKERRQ(ierr);
 
   /* Free workspace */
-  ierr = MatColoringDestroy(&coloring);CHKERRQ(ierr);
+  ierr = MatFDColoringDestroy(&fdcoloring);CHKERRQ(ierr);
   ierr = MatDestroy(&J);CHKERRQ(ierr);
   ierr = VecDestroy(&r);CHKERRQ(ierr);
   ierr = VecDestroy(&u);CHKERRQ(ierr);
