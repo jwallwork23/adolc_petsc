baff9c8b2add6fd32b5251cf4fe41e0366435ba7
jwallwork23 <j.wallwork16@imperial.ac.uk>
2018-09-13 13:24:41 -0500

    Use FormJacobian from snes ex1 for colouring test

--- petsc/colour/ex.c
+++ petsc/colour/ex.c
@@ -5,13 +5,40 @@ typedef struct {
   PetscReal c;
 } AppCtx;
 
-PetscErrorCode FormJacobian(SNES snes,Vec U,Mat J,Mat Jpre,void *ctx){
+/*
+  FormJacobian1 from snes ex1
 
-  PetscErrorCode ierr;
+  TODO: Consider something that actually has sparsity
+*/
+PetscErrorCode FormJacobian(SNES snes,Vec U,Mat J,Mat Jpre,void *ctx)
+{
+  PetscErrorCode    ierr;
+  const PetscScalar *u;
+  PetscScalar       A[2][2];
+  PetscInt          idx[2] = {0,1};
 
   PetscFunctionBegin;
 
-  // TODO
+  /* Get pointer to vector data */
+  ierr = VecGetArrayRead(U,&u);CHKERRQ(ierr);
+
+  /* Compute Jacobian entries and insert into matrix */
+  A[0][0] = 2.0*u[0]+u[1];
+  A[0][1] = u[0];
+  A[1][0] = u[1];
+  A[1][1] = u[0]+2.0*u[1];
+  ierr = MatSetValues(Jpre,2,idx,2,idx,A,INSERT_VALUES);CHKERRQ(ierr);
+
+  /* Restore vector */
+  ierr = VecRestoreArrayRead(U,&u);CHKERRQ(ierr);
+
+  /* Assemble matrix */
+  ierr = MatAssemblyBegin(Jpre,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(Jpre,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  if (J != Jpre) {
+    ierr = MatAssemblyBegin(J,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+    ierr = MatAssemblyEnd(J,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  }
 
   PetscFunctionReturn(0);
 }
@@ -38,7 +65,7 @@ int main(int argc,char **args)
     in the Jacobian can all be computed simultaneously.
   */
   ierr = MatColoringCreate(J,&coloring);CHKERRQ(ierr);
-  ierr = MatColoringSetType(coloring,MATCOLORINGSL);CHKERRQ(ierr);	// Use 'smallest last' type
+  ierr = MatColoringSetType(coloring,MATCOLORINGSL);CHKERRQ(ierr);	// Use 'smallest last' method
   ierr = MatColoringSetFromOptions(coloring);CHKERRQ(ierr);
   ierr = MatColoringApply(coloring,&iscoloring);CHKERRQ(ierr);
 
