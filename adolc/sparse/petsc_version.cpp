1995dd2d57d287029ea3283ffb02ad7f3ba3f617
jwallwork23 <jwallwork16@imperial.ac.uk>
2018-09-17 11:17:50 -0500

    It seems MatFDColoring is only an option for square matrices

--- adolc/sparse/petsc_version.cpp
+++ adolc/sparse/petsc_version.cpp
@@ -11,15 +11,12 @@ PetscErrorCode ActiveEvaluate(adouble *x,adouble *c);
 int main(int argc,char **args)
 {
   PetscErrorCode  ierr;
-  ISColoring      iscoloring;
-  MatColoring     coloring;
-  MatFDColoring   fdcoloring;
 
   ierr = PetscInitialize(&argc,&args,(char*)0,NULL);if (ierr) return ierr;
 
   PetscInt n=6,m=3,i,j;
-  PetscScalar x[6],c[3];
-  adouble xad[6],cad[3];
+  PetscScalar x[n],c[m];
+  adouble xad[n],cad[m];
 
 /****************************************************************************/
 /*******                function evaluation                   ***************/
@@ -48,12 +45,12 @@ int main(int argc,char **args)
 /********           For comparisons: Full Jacobian                   ********/
 /****************************************************************************/
 
-  PetscScalar **J;
-  J = myalloc2(m,n);
+  PetscScalar **Jdense;
+  Jdense = myalloc2(m,n);
 
-  jacobian(tag,m,n,x,J);
+  jacobian(tag,m,n,x,Jdense);
 
-  ierr = PrintMat(" J",m,n,J);CHKERRQ(ierr);
+  ierr = PrintMat(" J",m,n,Jdense);CHKERRQ(ierr);
   printf("\n");
 
 /****************************************************************************/
@@ -80,18 +77,70 @@ int main(int argc,char **args)
     printf(" %d: ",i);
     for (j=1;j<= (int) JP[i][0];j++)
       printf(" %d ",JP[i][j]);
-      printf("\n");
+    printf("\n");
   }
   printf("\n");
 
+/*--------------------------------------------------------------------------*/
+/*                                                     preallocate nonzeros */
+/*--------------------------------------------------------------------------*/
+
+  PetscInt        *dnz,*onz,*cols;
+
+  ierr = MatPreallocateInitialize(PETSC_COMM_WORLD,m,n,dnz,onz);CHKERRQ(ierr);
+  for (i=0;i<m;i++) {
+    ierr = PetscMalloc1(JP[i][0],&cols);CHKERRQ(ierr);
+    for (j=1;j<= (int) JP[i][0];j++) {
+      cols[j-1] = JP[i][j];
+    }
+    ierr = MatPreallocateSet(i,n,cols,dnz,onz);CHKERRQ(ierr);
+    ierr = PetscFree(cols);CHKERRQ(ierr);
+  }
+  ierr = MatPreallocateFinalize(dnz,onz);CHKERRQ(ierr);
+
+  // TODO: If it turns out this isn't doing the right job then try MatCreateSeqAIJ with nz and nnz
+
+/*--------------------------------------------------------------------------*/
+/*                                                            colour matrix */
+/*--------------------------------------------------------------------------*/
+
+  ISColoring      iscoloring;
+  MatColoring     coloring;
+  MatFDColoring   fdcoloring;
+  Mat             J;
+
+  // Create Jacobian object, assemnling with preallocated nonzeros
+  ierr = MatCreate(PETSC_COMM_WORLD,&J);CHKERRQ(ierr);
+  ierr = MatSetSizes(J,PETSC_DECIDE,PETSC_DECIDE,m,n);CHKERRQ(ierr);
+  ierr = MatSetFromOptions(J);CHKERRQ(ierr);
+  ierr = MatSetUp(J);CHKERRQ(ierr);
+  ierr = MatAssemblyBegin(J,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(J,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+
+  // Colour Jacobian
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"Creating colouring of J...\n");
+  ierr = MatColoringCreate(J,&coloring);CHKERRQ(ierr);
+  ierr = MatColoringSetType(coloring,MATCOLORINGSL);CHKERRQ(ierr);      // Use 'smallest last' method
+  ierr = MatColoringSetFromOptions(coloring);CHKERRQ(ierr);
+  ierr = MatColoringApply(coloring,&iscoloring);CHKERRQ(ierr);
+  ierr = MatFDColoringCreate(J,iscoloring,&fdcoloring);CHKERRQ(ierr);
+  ierr = MatFDColoringSetFromOptions(fdcoloring);CHKERRQ(ierr);
+  ierr = MatFDColoringSetUp(J,iscoloring,fdcoloring);CHKERRQ(ierr);
+
+
 /****************************************************************************/
-/*******       free memory and finalise                       ***************/
+/*******       free workspace and finalise                    ***************/
 /****************************************************************************/
+
+  ierr = MatFDColoringDestroy(&fdcoloring);CHKERRQ(ierr);
+  ierr = MatColoringDestroy(&coloring);CHKERRQ(ierr);
+  ierr = ISColoringDestroy(&iscoloring);CHKERRQ(ierr);
+  ierr = MatDestroy(&J);CHKERRQ(ierr);
 
   for (i=0;i<m;i++)
     free(JP[i]);
   free(JP);
-  myfree2(J);
+  myfree2(Jdense);
 
   ierr = PetscFinalize();
 
@@ -100,11 +149,9 @@ int main(int argc,char **args)
 
 PetscErrorCode PrintMat(const char* name,PetscInt m,PetscInt n,PetscScalar **M)
 {
-  PetscErrorCode ierr;
   PetscInt       i,j;
 
   PetscFunctionBegin;
-
   printf("%s \n",name);
   for(i=0; i<m ;i++) {
       printf("\n %d: ",i);
@@ -112,7 +159,6 @@ PetscErrorCode PrintMat(const char* name,PetscInt m,PetscInt n,PetscScalar **M)
           printf(" %10.4f ", M[i][j]);
   }
   printf("\n");
-
   PetscFunctionReturn(0);
 }
 
