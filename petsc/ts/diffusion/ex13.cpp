9a750d9181cb8e58b4fa53f668f993a3831e913c
jwallwork23 <jwallwork16@imperial.ac.uk>
2018-10-19 16:29:25 -0500

    Trace just once in ex13

--- petsc/ts/diffusion/ex13.cpp
+++ petsc/ts/diffusion/ex13.cpp
@@ -44,8 +44,8 @@ typedef struct {
 } AppCtx;
 
 /* (Slightly modified) functions included in original code of ex13.c */
-extern PetscErrorCode RHSFunction(TS,PetscReal,Vec,Vec,void*);
-extern PetscErrorCode RHSLocalPassive(DM da,PetscScalar **f,PetscScalar **uarray,void *ptr);
+extern PetscErrorCode RHSFunctionActive(TS,PetscReal,Vec,Vec,void*);
+extern PetscErrorCode RHSFunctionPassive(TS,PetscReal,Vec,Vec,void *ptr);
 extern PetscErrorCode RHSJacobianByHand(TS,PetscReal,Vec,Mat,Mat,void*);
 extern PetscErrorCode FormInitialSolution(DM,Vec,void*);
 
@@ -105,7 +105,7 @@ int main(int argc,char **argv)
   ierr = TSCreate(PETSC_COMM_WORLD,&ts);CHKERRQ(ierr);
   ierr = TSSetDM(ts,da);CHKERRQ(ierr);
   ierr = TSSetType(ts,TSBEULER);CHKERRQ(ierr);
-  ierr = TSSetRHSFunction(ts,r,RHSFunction,&user);CHKERRQ(ierr);
+  ierr = TSSetRHSFunction(ts,r,RHSFunctionPassive,&user);CHKERRQ(ierr);
 
   if (!adctx->no_an) {
 
@@ -140,6 +140,9 @@ int main(int argc,char **argv)
     user.u_a = u_a;
     user.f_a = f_a;
 
+    // Trace RHSFunction, so that ADOL-C has tape to read from
+    ierr = RHSFunctionActive(ts,1.0,u,r,&user);CHKERRQ(ierr);
+
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
       In the case where ADOL-C generates the Jacobian in compressed format,
       seed and recovery matrices are required. Since the sparsity structure
@@ -148,9 +151,6 @@ int main(int argc,char **argv)
       these objects once.
        - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
     if (adctx->sparse) {
-
-      // Trace RHSFunction, so that ADOL-C has tape to read from
-      ierr = RHSFunction(ts,1.0,u,r,&user);CHKERRQ(ierr);
 
       // Generate sparsity pattern and create an associated colouring
       ierr = PetscMalloc1(adctx->n,&u_vec);CHKERRQ(ierr);
@@ -250,95 +250,6 @@ int main(int argc,char **argv)
   return ierr;
 }
 
-
-PetscErrorCode RHSLocalActive(DM da,PetscScalar **f,PetscScalar **uarray,void *ptr)
-{
-  AppCtx         *user = (AppCtx*)ptr;
-  PetscErrorCode ierr;
-  PetscInt       i,j,xs,ys,xm,ym,gxs,gys,gxm,gym,Mx,My;
-  PetscReal      hx,hy,sx,sy,two = 2.0;
-  adouble        **f_a = user->f_a,**u_a = user->u_a;
-  adouble        u,uxx,uyy;
-
-  PetscFunctionBegin;
-  ierr = DMDAGetCorners(da,&xs,&ys,NULL,&xm,&ym,NULL);CHKERRQ(ierr);
-  ierr = DMDAGetGhostCorners(da,&gxs,&gys,NULL,&gxm,&gym,NULL);CHKERRQ(ierr);
-  ierr = DMDAGetInfo(da,PETSC_IGNORE,&Mx,&My,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE);CHKERRQ(ierr);
-  hx = 1.0/(PetscReal)(Mx-1); sx = 1.0/(hx*hx);
-  hy = 1.0/(PetscReal)(My-1); sy = 1.0/(hy*hy);
-
-  trace_on(1);  // ----------------------------------------------- Start of active section
-
-  /*
-    Mark independence
-
-    NOTE: Ghost points are marked as independent, in place of the points they represent on
-          other processors / on other boundaries.
-  */
-  for (j=gys; j<gys+gym; j++) {
-    for (i=gxs; i<gxs+gxm; i++)
-      u_a[j][i] <<= uarray[j][i];
-  }
-
-  /*
-    Compute function over the locally owned part of the grid
-  */
-  for (j=ys; j<ys+ym; j++) {
-    for (i=xs; i<xs+xm; i++) {
-      if (i == 0 || j == 0 || i == Mx-1 || j == My-1)  // Consider boundary cases
-        f_a[j][i] = u_a[j][i];
-      else {
-        u         = u_a[j][i];
-        uxx       = (-two*u + u_a[j][i-1] + u_a[j][i+1])*sx;
-        uyy       = (-two*u + u_a[j-1][i] + u_a[j+1][i])*sy;
-        f_a[j][i] = uxx + uyy;
-      }
-    }
-  }
-
-  /*
-    Mark dependence
-
-    NOTE: Ghost points are marked as dependent in order to vastly simplify index notation
-          during Jacobian assembly.
-  */
-  for (j=gys; j<gys+gym; j++) {
-    for (i=gxs; i<gxs+gxm; i++)
-      f_a[j][i] >>= f[j][i];
-  }
-  trace_off();  // ----------------------------------------------- End of active section
-
-  PetscFunctionReturn(0);
-}
-
-PetscErrorCode RHSLocalPassive(DM da,PetscScalar **f,PetscScalar **uarray,void *ptr)
-{
-  PetscErrorCode ierr;
-  PetscInt       i,j,xs,ys,xm,ym,Mx,My;
-  PetscReal      hx,hy,sx,sy,two=2.0;
-  PetscScalar    u,uxx,uyy;
-
-  PetscFunctionBegin;
-  ierr = DMDAGetCorners(da,&xs,&ys,NULL,&xm,&ym,NULL);CHKERRQ(ierr);
-  ierr = DMDAGetInfo(da,PETSC_IGNORE,&Mx,&My,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE);CHKERRQ(ierr);
-  hx = 1.0/(PetscReal)(Mx-1); sx = 1.0/(hx*hx);
-  hy = 1.0/(PetscReal)(My-1); sy = 1.0/(hy*hy);
-  for (j=ys; j<ys+ym; j++) {
-    for (i=xs; i<xs+xm; i++) {
-      if (i == 0 || j == 0 || i == Mx-1 || j == My-1) {
-        f[j][i] = uarray[j][i];
-        continue;
-      }
-      u       = uarray[j][i];
-      uxx     = (-two*u + uarray[j][i-1] + uarray[j][i+1])*sx;
-      uyy     = (-two*u + uarray[j-1][i] + uarray[j+1][i])*sy;
-      f[j][i] = uxx + uyy;
-    }
-  }
-
-  PetscFunctionReturn(0);
-}
-
 /* ------------------------------------------------------------------- */
 /*
    RHSFunction - Evaluates nonlinear function, F(u).
@@ -351,17 +262,22 @@ PetscErrorCode RHSLocalPassive(DM da,PetscScalar **f,PetscScalar **uarray,void *
    Output Parameter:
 .  F - function vector
  */
-PetscErrorCode RHSFunction(TS ts,PetscReal ftime,Vec U,Vec F,void *ptr)
+PetscErrorCode RHSFunctionPassive(TS ts,PetscReal ftime,Vec U,Vec F,void *ptr)
 {
-  AppCtx         *user=(AppCtx*)ptr;
   DM             da;
   PetscErrorCode ierr;
-  PetscInt       xm,ym;
+  PetscInt       i,j,xs,ys,xm,ym,Mx,My;
   PetscScalar    **u,**f;
   Vec            localU,localF;
+  PetscReal      hx,hy,sx,sy,two=2.0;
+  PetscScalar    uxx,uyy;
 
   PetscFunctionBeginUser;
   ierr = TSGetDM(ts,&da);CHKERRQ(ierr);
+  ierr = DMDAGetCorners(da,&xs,&ys,NULL,&xm,&ym,NULL);CHKERRQ(ierr);
+  ierr = DMDAGetInfo(da,PETSC_IGNORE,&Mx,&My,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE);CHKERRQ(ierr);
+  hx = 1.0/(PetscReal)(Mx-1); sx = 1.0/(hx*hx);
+  hy = 1.0/(PetscReal)(My-1); sy = 1.0/(hy*hy);
   ierr = DMGetLocalVector(da,&localU);CHKERRQ(ierr);
   ierr = DMGetLocalVector(da,&localF);CHKERRQ(ierr);
 
@@ -385,15 +301,121 @@ PetscErrorCode RHSFunction(TS ts,PetscReal ftime,Vec U,Vec F,void *ptr)
   ierr = DMDAGetCorners(da,NULL,NULL,NULL,&xm,&ym,NULL);CHKERRQ(ierr);
 
   /* Compute function over the locally owned part of the grid */
-  if (!user->adctx->no_an) {
-    ierr = RHSLocalActive(da,f,u,user);CHKERRQ(ierr);
-
-    /* Test zeroth order scalar evaluation in ADOL-C gives the same result */
-    if (user->adctx->zos) {
-      ierr = TestZOS2d(da,f,u,user->adctx->zos_view);CHKERRQ(ierr);
+  for (j=ys; j<ys+ym; j++) {
+    for (i=xs; i<xs+xm; i++) {
+      if (i == 0 || j == 0 || i == Mx-1 || j == My-1) {
+        f[j][i] = u[j][i];
+        continue;
+      }
+      uxx     = (-two*u[j][i] + u[j][i-1] + u[j][i+1])*sx;
+      uyy     = (-two*u[j][i] + u[j-1][i] + u[j+1][i])*sy;
+      f[j][i] = uxx + uyy;
     }
-  } else {
-    ierr = RHSLocalPassive(da,f,u,user);CHKERRQ(ierr);
+  }
+
+  /*
+     Gather global vector, using the 2-step process
+        DMLocalToGlobalBegin(),DMLocalToGlobalEnd().
+  */
+  //ierr = DMLocalToGlobalBegin(da,localF,ADD_VALUES,F);CHKERRQ(ierr);
+  //ierr = DMLocalToGlobalEnd(da,localF,ADD_VALUES,F);CHKERRQ(ierr);
+  ierr = DMLocalToGlobalBegin(da,localF,INSERT_VALUES,F);CHKERRQ(ierr);
+  ierr = DMLocalToGlobalEnd(da,localF,INSERT_VALUES,F);CHKERRQ(ierr);
+
+  /* Restore vectors */
+  ierr = DMDAVecRestoreArray(da,localF,&f);CHKERRQ(ierr);
+  ierr = DMDAVecRestoreArrayRead(da,localU,&u);CHKERRQ(ierr);
+  ierr = DMRestoreLocalVector(da,&localF);CHKERRQ(ierr);
+  ierr = DMRestoreLocalVector(da,&localU);CHKERRQ(ierr);
+  ierr = PetscLogFlops(11.0*xm*ym);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+PetscErrorCode RHSFunctionActive(TS ts,PetscReal ftime,Vec U,Vec F,void *ptr)
+{
+  AppCtx         *user=(AppCtx*)ptr;
+  DM             da;
+  PetscErrorCode ierr;
+  PetscScalar    **u,**f;
+  Vec            localU,localF;
+  PetscInt       i,j,xs,ys,xm,ym,gxs,gys,gxm,gym,Mx,My;
+  PetscReal      hx,hy,sx,sy,two = 2.0;
+  adouble        **f_a = user->f_a,**u_a = user->u_a;
+  adouble        uxx,uyy;
+
+  PetscFunctionBeginUser;
+  ierr = TSGetDM(ts,&da);CHKERRQ(ierr);
+  ierr = DMDAGetCorners(da,&xs,&ys,NULL,&xm,&ym,NULL);CHKERRQ(ierr);
+  ierr = DMDAGetGhostCorners(da,&gxs,&gys,NULL,&gxm,&gym,NULL);CHKERRQ(ierr);
+  ierr = DMDAGetInfo(da,PETSC_IGNORE,&Mx,&My,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE,PETSC_IGNORE);CHKERRQ(ierr);
+  hx = 1.0/(PetscReal)(Mx-1); sx = 1.0/(hx*hx);
+  hy = 1.0/(PetscReal)(My-1); sy = 1.0/(hy*hy);
+
+  ierr = DMGetLocalVector(da,&localU);CHKERRQ(ierr);
+  ierr = DMGetLocalVector(da,&localF);CHKERRQ(ierr);
+
+  /*
+     Scatter ghost points to local vector,using the 2-step process
+        DMGlobalToLocalBegin(),DMGlobalToLocalEnd().
+     By placing code between these two statements, computations can be
+     done while messages are in transition.
+  */
+  ierr = DMGlobalToLocalBegin(da,U,INSERT_VALUES,localU);CHKERRQ(ierr);
+  ierr = DMGlobalToLocalEnd(da,U,INSERT_VALUES,localU);CHKERRQ(ierr);
+  //ierr = VecZeroEntries(F);
+  ierr = DMGlobalToLocalBegin(da,F,INSERT_VALUES,localF);CHKERRQ(ierr);
+  ierr = DMGlobalToLocalEnd(da,F,INSERT_VALUES,localF);CHKERRQ(ierr);
+
+  /* Get pointers to vector data */
+  ierr = DMDAVecGetArrayRead(da,localU,&u);CHKERRQ(ierr);
+  ierr = DMDAVecGetArray(da,localF,&f);CHKERRQ(ierr);
+
+  /* Get local grid boundaries */
+  ierr = DMDAGetCorners(da,NULL,NULL,NULL,&xm,&ym,NULL);CHKERRQ(ierr);
+
+  trace_on(1);  // ----------------------------------------------- Start of active section
+
+  /*
+    Mark independence
+
+    NOTE: Ghost points are marked as independent, in place of the points they represent on
+          other processors / on other boundaries.
+  */
+  for (j=gys; j<gys+gym; j++) {
+    for (i=gxs; i<gxs+gxm; i++)
+      u_a[j][i] <<= u[j][i];
+  }
+
+  /*
+    Compute function over the locally owned part of the grid
+  */
+  for (j=ys; j<ys+ym; j++) {
+    for (i=xs; i<xs+xm; i++) {
+      if (i == 0 || j == 0 || i == Mx-1 || j == My-1)  // Consider boundary cases
+        f_a[j][i] = u_a[j][i];
+      else {
+        uxx       = (-two*u_a[j][i] + u_a[j][i-1] + u_a[j][i+1])*sx;
+        uyy       = (-two*u_a[j][i] + u_a[j-1][i] + u_a[j+1][i])*sy;
+        f_a[j][i] = uxx + uyy;
+      }
+    }
+  }
+
+  /*
+    Mark dependence
+
+    NOTE: Ghost points are marked as dependent in order to vastly simplify index notation
+          during Jacobian assembly.
+  */
+  for (j=gys; j<gys+gym; j++) {
+    for (i=gxs; i<gxs+gxm; i++)
+      f_a[j][i] >>= f[j][i];
+  }
+  trace_off();  // ----------------------------------------------- End of active section
+
+  /* Test zeroth order scalar evaluation in ADOL-C gives the same result */
+  if (user->adctx->zos) {
+    ierr = TestZOS2d(da,f,u,user->adctx->zos_view);CHKERRQ(ierr);
   }
 
   /*
