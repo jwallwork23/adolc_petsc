cce696afbfa1921034363f3126cbf6f4c50489f1
jwallwork23 <jwallwork16@imperial.ac.uk>
2018-10-29 17:13:26 -0500

    Tangent mode in petsc

--- tapenade/blas/petsc/test.c
+++ tapenade/blas/petsc/test.c
@@ -20,10 +20,12 @@ int main(int argc,char **argv)
   PetscBLASInt   m=2,n=2,k=2;
   PetscScalar    alpha=1.,beta=0.;
   PetscScalar    *A,*B,*C;
+  PetscScalar    *Ad,*Bd,*Cd;
   PetscInt       i,j;
 
   ierr = PetscInitialize(&argc,&argv,"petscoptions",NULL);CHKERRQ(ierr);
   ierr = PetscMalloc3(m*k,&A,k*n,&B,m*n,&C);CHKERRQ(ierr);
+  ierr = PetscMalloc3(m*k,&Ad,k*n,&Bd,m*n,&Cd);CHKERRQ(ierr);
 
   for (i=0; i<m; ++i) {
     for (j=0; j<k; ++j) {
@@ -31,15 +33,30 @@ int main(int argc,char **argv)
     }
   }
   printmat("A:",m,k,A);
+  for (i=0; i<m; ++i) {
+    for (j=0; j<k; ++j) {
+      Ad[i+j*k] = i;
+    }
+  }
+  printmat("Ad:",m,k,Ad);
   for (i=0; i<k; ++i) {
     for (j=0; j<n; ++j) {
       B[i+j*n] = j+1;
     }
   }
   printmat("B:",k,n,B);
-  ierr = PetscDGEMM("N","N",&m,&n,&k,&alpha,A,&m,B,&k,&beta,C,&m);CHKERRQ(ierr);
-  printmat("C:",m,n,C);
+  for (i=0; i<k; ++i) {
+    for (j=0; j<n; ++j) {
+      Bd[i+j*n] = j;
+    }
+  }
+  printmat("Bd:",k,n,Bd);
 
+  ierr = PetscDGEMMForward("N","N",&m,&n,&k,&alpha,A,Ad,&m,B,Bd,&k,&beta,C,Cd,&m);CHKERRQ(ierr);
+  printmat("C:",m,n,C);
+  printmat("Cd:",m,n,Cd);
+
+  ierr = PetscFree3(Ad,Bd,Cd);CHKERRQ(ierr);
   ierr = PetscFree3(A,B,C);CHKERRQ(ierr);
   ierr = PetscFinalize();
   return ierr;
