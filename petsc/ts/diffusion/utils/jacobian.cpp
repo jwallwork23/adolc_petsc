b2dcd5cb6deaa1e5eedf9c3d0695509076c979b4
jwallwork23 <jwallwork16@imperial.ac.uk>
2018-10-31 10:37:48 -0500

    Forgot to add utils

---
+++ petsc/ts/diffusion/utils/jacobian.cpp
@@ -0,0 +1,119 @@
+#include "tracing.cpp"
+
+/* --------------------------------------------------------------------- */
+/*
+   RHSJacobianByHand - User-provided routine to compute the Jacobian of
+   the nonlinear right-hand-side function of the ODE, as given in ex13.c.
+
+   Input Parameters:
+   ts - the TS context
+   t - current time
+   U - global input vector
+   ctx - optional user-defined context, as set by TSetRHSJacobian()
+
+   Output Parameters:
+   J - Jacobian matrix
+   Jpre - optionally different preconditioning matrix
+*/
+PetscErrorCode RHSJacobianByHand(TS ts,PetscReal t,Vec U,Mat J,Mat Jpre,void *ctx)
+{
+  PetscErrorCode ierr;
+  DM             da;
+  DMDALocalInfo  info;
+  PetscInt       i,j;
+  PetscReal      hx,hy,sx,sy;
+
+  PetscFunctionBeginUser;
+  ierr = TSGetDM(ts,&da);CHKERRQ(ierr);
+  ierr = DMDAGetLocalInfo(da,&info);CHKERRQ(ierr);
+  hx   = 1.0/(PetscReal)(info.mx-1); sx = 1.0/(hx*hx);
+  hy   = 1.0/(PetscReal)(info.my-1); sy = 1.0/(hy*hy);
+  for (j=info.ys; j<info.ys+info.ym; j++) {
+    for (i=info.xs; i<info.xs+info.xm; i++) {
+      PetscInt    nc = 0;
+      MatStencil  row,col[5];
+      PetscScalar val[5];
+      row.i = i; row.j = j;
+      if (i == 0 || j == 0 || i == info.mx-1 || j == info.my-1) {
+        col[nc].i = i; col[nc].j = j; val[nc++] = 1.0;
+      } else {
+        col[nc].i = i-1; col[nc].j = j;   val[nc++] = sx;
+        col[nc].i = i+1; col[nc].j = j;   val[nc++] = sx;
+        col[nc].i = i;   col[nc].j = j-1; val[nc++] = sy;
+        col[nc].i = i;   col[nc].j = j+1; val[nc++] = sy;
+        col[nc].i = i;   col[nc].j = j;   val[nc++] = -2*sx - 2*sy;
+      }
+      ierr = MatSetValuesStencil(Jpre,1,&row,nc,col,val,INSERT_VALUES);CHKERRQ(ierr);
+    }
+  }
+  ierr = MatAssemblyBegin(Jpre,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(Jpre,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  if (J != Jpre) {
+    ierr = MatAssemblyBegin(J,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+    ierr = MatAssemblyEnd(J,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  }
+  PetscFunctionReturn(0);
+}
+
+/* --------------------------------------------------------------------- */
+/*
+   RHSJacobianAdolc - Automatically generated ADOL-C version.
+
+   Input Parameters:
+   ts - the TS context
+   t - current time
+   U - global input vector
+   ctx - optional user-defined context, as set by TSetRHSJacobian()
+
+   Output Parameters:
+   J - Jacobian matrix
+   Jpre - optionally different preconditioning matrix
+*/
+PetscErrorCode RHSJacobianAdolc(TS ts,PetscReal t,Vec U,Mat J,Mat Jpre,void *ctx)
+{
+  AppCtx         *appctx = (AppCtx*)ctx;
+  PetscErrorCode ierr;
+  DM             da;
+  PetscScalar    *u_vec;
+  Vec            localU;
+
+  PetscFunctionBeginUser;
+  ierr = TSGetDM(ts,&da);CHKERRQ(ierr);
+  ierr = DMGetLocalVector(da,&localU);CHKERRQ(ierr);
+
+  /*
+     Scatter ghost points to local vector,using the 2-step process
+        DMGlobalToLocalBegin(),DMGlobalToLocalEnd().
+     By placing code between these two statements, computations can be
+     done while messages are in transition.
+  */
+  ierr = DMGlobalToLocalBegin(da,U,INSERT_VALUES,localU);CHKERRQ(ierr);
+  ierr = DMGlobalToLocalEnd(da,U,INSERT_VALUES,localU);CHKERRQ(ierr);
+
+  /* Get pointers to vector data */
+  ierr = VecGetArray(localU,&u_vec);CHKERRQ(ierr);
+
+  /*
+    Calculate Jacobian using ADOL-C
+  */
+  ierr = AdolcComputeRHSJacobian(J,u_vec,appctx->adctx);CHKERRQ(ierr);
+
+  /*
+    Restore vectors
+  */
+  ierr = VecRestoreArray(localU,&u_vec);CHKERRQ(ierr);
+  ierr = DMRestoreLocalVector(da,&localU);CHKERRQ(ierr);
+
+  /*
+    Assemble local matrix
+  */
+  ierr = MatAssemblyBegin(Jpre,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(Jpre,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  if (J != Jpre) {
+    ierr = MatAssemblyBegin(J,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+    ierr = MatAssemblyEnd(J,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  }
+
+  PetscFunctionReturn(0);
+}
+
