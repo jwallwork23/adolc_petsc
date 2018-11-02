f8e1e82abfcf3011105c100072da1e35de6541dc
jwallwork23 <j.wallwork16@imperial.ac.uk>
2018-11-02 10:30:19 -0500

    Working forward traceless matfree example

--- adolc/sparse/traceless.cpp
+++ adolc/sparse/traceless.cpp
@@ -8,7 +8,7 @@ extern PetscErrorCode JacobianVectorProduct(Mat J,Vec x,Vec y);
 
 typedef struct {
   PetscScalar *X;    // Independent variable values
-  PetscScalar *Y;    // Dependent variable values
+  Vec         Y;     // Dependent variable values
 } AdolcCtx;
 
 int main(int argc,char **args)
@@ -17,7 +17,7 @@ int main(int argc,char **args)
   PetscErrorCode  ierr;
   MPI_Comm        comm = MPI_COMM_WORLD;
   PetscInt        n = 6,m = 3,i,ni[n];
-  Vec             W,X,Y;
+  Vec             W,X;
   Mat             J;
 
   ierr = PetscInitialize(&argc,&args,(char*)0,NULL);if (ierr) return ierr;
@@ -37,9 +37,9 @@ int main(int argc,char **args)
   //ierr = VecView(X,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
 
   /* Vec for dependent variables */
-  ierr = VecCreate(comm,&Y);CHKERRQ(ierr);
-  ierr = VecSetSizes(Y,PETSC_DECIDE,m);CHKERRQ(ierr);
-  ierr = VecSetFromOptions(Y);CHKERRQ(ierr);
+  ierr = VecCreate(comm,&ctx.Y);CHKERRQ(ierr);
+  ierr = VecSetSizes(ctx.Y,PETSC_DECIDE,m);CHKERRQ(ierr);
+  ierr = VecSetFromOptions(ctx.Y);CHKERRQ(ierr);
 
   /* Create matrix free matrix */
   ierr = MatCreateShell(comm,m,n,m,n,NULL,&J);CHKERRQ(ierr);
@@ -56,23 +56,24 @@ int main(int argc,char **args)
   ierr = MatMult(J,X,W);CHKERRQ(ierr);
 
   /* Print results */
-  //ierr = PetscPrintf(comm,"\nFunction evaluation : \n");CHKERRQ(ierr);
-  //ierr = VecView(Y,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+  ierr = PetscPrintf(comm,"\nFunction evaluation : \n");CHKERRQ(ierr);
+  ierr = VecView(ctx.Y,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
   ierr = PetscPrintf(comm,"\nJacobian vector product :\n");CHKERRQ(ierr);
   ierr = VecView(W,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
 
   ierr = VecDestroy(&W);CHKERRQ(ierr);
   ierr = MatDestroy(&J);CHKERRQ(ierr);
-  ierr = VecDestroy(&Y);CHKERRQ(ierr);
+  ierr = VecDestroy(&ctx.Y);CHKERRQ(ierr);
   ierr = VecDestroy(&X);CHKERRQ(ierr);
   ierr = PetscFree(ctx.X);CHKERRQ(ierr);
+
   ierr = PetscFinalize();
 
   return ierr;
 }
 
 /*
-  ADOL-C implementation for Jacobian vector product, using the forward mode of AD.
+  ADOL-C implementation for Jacobian vector product, using the traceless forward mode of AD.
   Intended to overload MatMult in matrix-free methods
 
   Input parameters:
@@ -87,38 +88,38 @@ PetscErrorCode JacobianVectorProduct(Mat J,Vec x_ro,Vec y)
   AdolcCtx          *ctx;
   PetscErrorCode    ierr;
   PetscInt          m,n,i;
-  PetscScalar       *action,*xdat,tmp;
-  adouble           xad[n],yad[m];
+  PetscScalar       *xdat,tmp;
+  adouble           *xad,*yad;
   Vec               x;
 
   PetscFunctionBegin;
 
   /* Read data and allocate memory */
   ierr = MatGetSize(J,&m,&n);CHKERRQ(ierr);
+  xad = new adouble[n];
+  yad = new adouble[m];
   ierr = MatShellGetContext(J,&ctx);CHKERRQ(ierr);
-  ierr = PetscMalloc1(m,&action);CHKERRQ(ierr);
   ierr = VecDuplicate(x_ro,&x);CHKERRQ(ierr);
   ierr = VecCopy(x_ro,x);CHKERRQ(ierr);
   ierr = VecGetArray(x,&xdat);CHKERRQ(ierr);
 
   /* Call function c(x) and apply Jacobian vector product*/
   for(i=0; i<n; i++) {
-    xad[i] = ctx->X[i];
-    tmp = xdat[i];
-    xad[i].setADValue(&tmp);
-    std::cout << "xad[" << i << "] = " << xad[i] << std::endl;
+    xad[i].setValue(ctx->X[i]);
+    xad[i].setADValue(&xdat[i]);
   }
   ierr = ActiveEvaluate(xad,yad);CHKERRQ(ierr);
   for(i=0; i<m; i++) {
-    //ctx->Y[i] = yad[i].getValue();
-    action = (PetscScalar*) yad[i].getADValue();
-    ierr = VecSetValues(y,1,&i,action,INSERT_VALUES);CHKERRQ(ierr);
+    tmp = yad[i].getValue();
+    ierr = VecSetValues(ctx->Y,1,&i,&tmp,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValues(y,1,&i,(PetscScalar*) yad[i].getADValue(),INSERT_VALUES);CHKERRQ(ierr);
   }
 
   /* Restore arrays and free memory */
   ierr = VecRestoreArray(x,&xdat);CHKERRQ(ierr);
   ierr = VecDestroy(&x);CHKERRQ(ierr);
-  ierr = PetscFree(action);CHKERRQ(ierr);
+  delete[] yad;
+  delete [] xad;
   PetscFunctionReturn(0);
 }
 
