deca36ad9a50830e4a3b3992e97ec3b4998e24b2
jwallwork23 <jwallwork16@imperial.ac.uk>
2018-11-06 12:48:36 -0600

    Reverse BLAS derivatives w.r.t. indiv args

--- blas/petsc/dgemm_deriv.c
+++ blas/petsc/dgemm_deriv.c
@@ -2,7 +2,7 @@
 
 
 /*
-  Forward derivative of BLASgemm w.r.t. both A and B.
+  Forward derivative of BLASgemm w.r.t. both matrix inputs.
 
   The derivative code was initially obtained by transformation of the LAPACK Fortran source code
   using the Tapenade automatic differentiation tool. This was then re-interpreted in terms of calls
@@ -26,9 +26,9 @@ PetscErrorCode GEMMDot(const char* TRANSA,const char* TRANSB,PetscBLASInt *M,Pet
 }
 
 /*
-  Forward derivative of BLASgemm w.r.t. first input, A.
+  Forward derivative of BLASgemm w.r.t. first matrix input, A.
 */
-PetscErrorCode GEMMDotLeft(const char* TRANSA,const char* TRANSB,PetscBLASInt *M,PetscBLASInt *N,PetscBLASInt *K,PetscScalar *ALPHA,PetscScalar *A,PetscScalar *AD,PetscBLASInt *LDA,PetscScalar *B,PetscBLASInt *LDB,PetscScalar *BETA,PetscScalar *C,PetscScalar *CD,PetscBLASInt *LDC)
+PetscErrorCode GEMMDotA(const char* TRANSA,const char* TRANSB,PetscBLASInt *M,PetscBLASInt *N,PetscBLASInt *K,PetscScalar *ALPHA,PetscScalar *A,PetscScalar *AD,PetscBLASInt *LDA,PetscScalar *B,PetscBLASInt *LDB,PetscScalar *BETA,PetscScalar *C,PetscScalar *CD,PetscBLASInt *LDC)
 {
   PetscScalar    one = 1.;
   PetscInt       *m = (PetscInt*) M,*n = (PetscInt*) N,i;
@@ -45,9 +45,9 @@ PetscErrorCode GEMMDotLeft(const char* TRANSA,const char* TRANSB,PetscBLASInt *M
 }
 
 /*
-  Forward derivative of BLASgemm w.r.t. second input, B.
+  Forward derivative of BLASgemm w.r.t. second matrix input, B.
 */
-PetscErrorCode GEMMDotRight(const char* TRANSA,const char* TRANSB,PetscBLASInt *M,PetscBLASInt *N,PetscBLASInt *K,PetscScalar *ALPHA,PetscScalar *A,PetscBLASInt *LDA,PetscScalar *B,PetscScalar *BD,PetscBLASInt *LDB,PetscScalar *BETA,PetscScalar *C,PetscScalar *CD,PetscBLASInt *LDC)
+PetscErrorCode GEMMDotB(const char* TRANSA,const char* TRANSB,PetscBLASInt *M,PetscBLASInt *N,PetscBLASInt *K,PetscScalar *ALPHA,PetscScalar *A,PetscBLASInt *LDA,PetscScalar *B,PetscScalar *BD,PetscBLASInt *LDB,PetscScalar *BETA,PetscScalar *C,PetscScalar *CD,PetscBLASInt *LDC)
 {
   PetscScalar    one = 1.;
   PetscInt       *m = (PetscInt*) M,*n = (PetscInt*) N,i;
@@ -64,7 +64,7 @@ PetscErrorCode GEMMDotRight(const char* TRANSA,const char* TRANSB,PetscBLASInt *
 }
 
 /*
-  Reverse derivative of BLASgemm.
+  Reverse derivative of BLASgemm w.r.t. both matrix inputs.
 
   The derivative code was initially obtained by transformation of the LAPACK Fortran source code
   using the Tapenade automatic differentiation tool. This was then re-interpreted in terms of calls
@@ -92,6 +92,63 @@ PetscErrorCode GEMMBar(const char* TRANSA,const char* TRANSB,PetscBLASInt *M,Pet
     leaveA = trans[0];
     BLASgemm_(TRANSB,&trans[1],K,M,N,ALPHA,B,LDB,CB,LDC,&one,AB,LDA);
   }
+
+  /* Reverse derivative for B */
+  if (*TRANSB == trans[0])
+    BLASgemm_(&leaveA,&trans[0],M,N,K,ALPHA,A,LDA,CB,LDC,&one,BB,LDB);
+  else
+    BLASgemm_(&trans[1],TRANSA,N,M,K,ALPHA,CB,LDC,A,LDA,&one,BB,LDB);
+
+  /* Quick return scaled C */
+  BLASgemm_(&trans[0],&trans[0],M,N,K,&zero,A,LDA,B,LDB,BETA,CB,LDC);
+  PetscFunctionReturn(0);
+}
+
+/*
+  Reverse derivative of BLASgemm w.r.t. first matrix input, A.
+*/
+PetscErrorCode GEMMBarA(const char* TRANSA,const char* TRANSB,PetscBLASInt *M,PetscBLASInt *N,PetscBLASInt *K,PetscScalar *ALPHA,PetscScalar *A,PetscScalar *AB,PetscBLASInt *LDA,PetscScalar *B,,PetscBLASInt *LDB,PetscScalar *BETA,PetscScalar *C,PetscScalar *CB,PetscBLASInt *LDC)
+{
+  PetscScalar    one = 1.,zero = 0.;
+  PetscInt       *m = (PetscInt*) M,*n = (PetscInt*) N,*k = (PetscInt*) K,i;
+  char           leaveB,trans[2] = "NT";
+
+  PetscFunctionBegin;
+  if (*TRANSB == trans[0])
+    leaveB = trans[1];
+  else
+    leaveB = trans[0];
+  for (i=0; i<(*m)*(*k); ++i) AB[i] = 0.;
+
+  /* Reverse derivative for A */
+  if (*TRANSA == trans[0]) {
+    leaveA = trans[1];
+    BLASgemm_(&trans[0],&leaveB,M,K,N,ALPHA,CB,LDC,B,LDB,&one,AB,LDA);
+  } else {
+    leaveA = trans[0];
+    BLASgemm_(TRANSB,&trans[1],K,M,N,ALPHA,B,LDB,CB,LDC,&one,AB,LDA);
+  }
+
+  /* Quick return scaled C */
+  BLASgemm_(&trans[0],&trans[0],M,N,K,&zero,A,LDA,B,LDB,BETA,CB,LDC);
+  PetscFunctionReturn(0);
+}
+
+/*
+  Reverse derivative of BLASgemm w.r.t. second matrix input, B.
+*/
+PetscErrorCode GEMMBarB(const char* TRANSA,const char* TRANSB,PetscBLASInt *M,PetscBLASInt *N,PetscBLASInt *K,PetscScalar *ALPHA,PetscScalar *A,PetscBLASInt *LDA,PetscScalar *B,PetscScalar *BB,PetscBLASInt *LDB,PetscScalar *BETA,PetscScalar *C,PetscScalar *CB,PetscBLASInt *LDC)
+{
+  PetscScalar    one = 1.,zero = 0.;
+  PetscInt       *m = (PetscInt*) M,*n = (PetscInt*) N,*k = (PetscInt*) K,i;
+  char           leaveA,trans[2] = "NT";
+
+  PetscFunctionBegin;
+  if (*TRANSA == trans[0])
+    leaveA = trans[1];
+  else
+    leaveA = trans[0];
+  for (i=0; i<(*k)*(*n); ++i) BB[i] = 0.;
 
   /* Reverse derivative for B */
   if (*TRANSB == trans[0])
