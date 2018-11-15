3385a83098a93c5b5c3d461ad713990f7de73303
jwallwork23 <jwallwork16@imperial.ac.uk>
2018-11-15 14:33:09 -0600

    Improvements and generality in plotting

--- petsc/ts/gray_scott/plot_results.m
+++ petsc/ts/gray_scott/plot_results.m
@@ -1,4 +1,4 @@
-function []=plot_results(N)
+function []=plot_results(n)
 % Plotting script for Gray-Scott problem
 %
 % To run, activate Octave and call plot_results(N), where N is the number of grid points used in
@@ -10,36 +10,41 @@ function []=plot_results(N)
 % -ts_max_steps 100 -ts_trajectory_type memory
 % -malloc_hbw
 
-fontsize_labels = 14;
-fontsize_grid   = 12;
-fontname = 'Times';
-
-ncores = [4,16,64];
-N = int2str(N);
-
-fbyhand = fopen(strcat('data/byhand',N,'.txt'), 'r');
-byhand = fscanf(fbyhand,'%f');
-fclose(fbyhand);
-
-fsparse = fopen(strcat('data/sparse',N,'.txt'), 'r');
-sparse = fscanf(fsparse,'%f');
-fclose(fsparse);
-
-fmatfree = fopen(strcat('data/matfree',N,'.txt'), 'r');
-matfree = fscanf(fmatfree,'%f');
-fclose(fmatfree);
-
-hold off
 clf
-set(gca,'FontName',fontname)
-set(gca,'FontSize',fontsize_grid)
-set(gca,'FontSize',fontsize_labels)
-loglog(ncores,byhand,'-+',ncores,sparse,'-o',ncores,matfree,'-*','Markersize',6,'LineWidth',2);
+set(gca,'FontName','Times')
+set(gca,'FontSize',12)
+set(gca,'FontSize',14)
+N = int2str(n);
+
+byhand = load(strcat('data/byhand',N,'.txt'));
+ncores = byhand(:,1);
+idx = ~isnan(byhand(:,2));
+loglog(ncores(idx,1),byhand(idx,2),'-+');
 hold on
-legend('Hand-coded','Sparse ADOL-C','Matrix-free ADOL-C','Location','NorthEast');
+
+if n == 65
+    full = load(strcat('data/full',N,'.txt'));
+    idx = ~isnan(full(:,2));
+    loglog(ncores(idx,1),full(idx,2),'-^');
+end
+
+sparse = load(strcat('data/sparse',N,'.txt'));
+idx = ~isnan(sparse(:,2));
+loglog(ncores(idx,1),sparse(idx,2),'-o');
+
+matfree = load(strcat('data/matfree',N,'.txt'));
+idx = ~isnan(matfree(:,2));
+loglog(ncores(idx,1),matfree(idx,2),'-*');
+
+if n == 65
+    legend('Hand-coded','ADOL-C','Sparse ADOL-C','Matrix-free ADOL-C','Location','NorthEast');
+else
+    %loglog(ncores_byhand,byhand,'-+',ncores_sparse,sparse,'-o',ncores_matfree,matfree,'-*','Markersize',6,'LineWidth',2);
+    legend('Hand-coded','Sparse ADOL-C','Matrix-free ADOL-C','Location','NorthEast');
+end
 xlabel('Number of cores');
 ylabel('Runtime (s)');
-title('Gray-Scott problem on KNL with checkpointing')
+title(strcat('Gray-Scott problem on ',{' '},N,'x',N,' grid with checkpointing'))
 saveas(gcf,outfile = strcat('plots/res',N,'.png'));
 
 end
